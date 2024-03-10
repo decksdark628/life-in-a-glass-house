13dd644b2dd8455de3499fc1afeb1ebbe9bd12dd
DecksDark <patrickdiazgrandez@gmail.com>
2024-03-10 18:59:56 -0500

    Quartz sync: Mar 10, 2024, 6:59 PM

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -7,8 +7,8 @@ export const sharedPageComponents: SharedLayout = {
   header: [],
   footer: Component.Footer({
     links: {
-      GitHub: "https://github.com/jackyzha0/quartz",
-      "Discord Community": "https://discord.gg/cRFFHYye7t",
+      "Website": "https://decksdark628.github.io/life-in-a-glass-house/",
+      "Youtube": "https://www.youtube.com/@decksdark",
     },
   }),
 }
@@ -26,11 +26,11 @@ export const defaultContentPageLayout: PageLayout = {
     Component.MobileOnly(Component.Spacer()),
     Component.Search(),
     Component.Darkmode(),
+    Component.TableOfContents(),
     Component.DesktopOnly(Component.Explorer()),
   ],
   right: [
-    Component.Graph(),
-    Component.DesktopOnly(Component.TableOfContents()),
+    Component.DesktopOnly(Component.Grap()),
     Component.Backlinks(),
   ],
 }
